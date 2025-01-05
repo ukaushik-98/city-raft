28302045c9c0d39a371ea500ba1df634fb1f41c1
ukaushik-98 <kaushikuk101@gmail.com>
2025-01-05 09:34:09 -0800

    add docs for rpcs

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -10,6 +10,8 @@ pub enum RPC {
     RequestVoteRPCResponse(RequestVoteRPCResponse),
 }
 
+/// Heartbeat struct used by leaders
+/// leader -> follower
 #[derive(Debug)]
 struct HeartBeatRPC {
     term: usize,
@@ -18,38 +20,66 @@ struct HeartBeatRPC {
     last_log_term: usize,
 }
 
+/// Response to heartbeat
+/// follower -> leader
 #[derive(Debug)]
 struct HeartBeatRPCResponse {
     term: usize,
     success: bool,
 }
 
+/// Vote Request RPC used by candidates
+/// candidate -> follower
 #[derive(Debug)]
 struct RequestVoteRPC {
+    /// candidate's term
     term: usize,
+    /// candidate requesting vote
     candidate_id: usize,
+    /// index of candidate's last log entry
     last_log_index: usize,
+    /// term of cnadidate's last log entry
     last_log_term: usize,
 }
 
+/// Response to RequestVoteRPC
+/// follower -> candidate
 #[derive(Debug)]
 struct RequestVoteRPCResponse {
+    /// currentTerm, for candidate to update itself
     term: usize,
+    /// true means candidate received vote
     vote_granted: bool,
 }
 
+/// Invoked by leader to replicate log entries
+/// Matches struct for HeartBeat
 #[derive(Debug)]
 pub struct AppendRPC {
+    /// leader's term
     term: usize,
+    /// so follower can redirect clients
     leader_id: usize,
+    /// index of log entry immediately preceding
+    /// new ones
     prev_log_index: usize,
+    /// term of prevLogIndex entry
+    prev_log_term: usize,
+    /// log entries to store (empty for heartbeat;
+    /// may send more than one for efficiency)
     entries: Vec<LogEntries>,
+    /// leader's commitIndex
     leader_commit: usize,
 }
 
+/// Response to AppendRPC
+/// follwer -> leader
 #[derive(Debug)]
 struct AppendRPCResponse {
+    /// currentTerm, for leader to update itsel
     term: usize,
+    /// true if follower contained entry matching
+    /// prevLogIndex and prevLogTerm
     success: bool,
 }
 
